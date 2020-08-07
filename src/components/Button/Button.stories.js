3f9a2fdd7355d17bff7b80b9a514a3d6c340e260
js2uix <deshineplus@naver.com>
2020-08-07 16:36:07 +0900

    storybook for react

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './index';
+
+export default {
+  title: 'Button Component',
+  component: Button,
+};
+
+export const ButtonComponent = () => <Button />;
+
+ButtonComponent.story = {
+  name: 'Button',
+};
---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button () {
+  return (
+    <button type="button">Hello!1</button>
+  );
+}
+
+export default Button;
