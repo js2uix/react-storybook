09906c3452991a1a539386867f6b5e416b7b0304
js2uix <deshineplus@naver.com>
2020-08-10 18:57:04 +0900

    storybook react test

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
 import Button from './index';
 
 export default {
   title: 'Button Component',
   component: Button,
+  decorators: [withKnobs],
 };
 
-export const ButtonComponent = () => <Button />;
+export const ButtonComponent = () => {
+  let onActive = false;
+
+  return (
+    <Button 
+      title={text('타이틀', '안녕하세요')} 
+      active={boolean('버튼 확성', false)}
+      onActiveHandler={action('클릭', !onActive)}
+    />
+  )
+};
 
 ButtonComponent.story = {
   name: 'Button',
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
-function Button () {
+import './Button.css'
+
+function Button ({title, active, onActiveHandler}) {
+
+  const [onActive, setActive] = useState(active);
+
+  /**
+   * setActiveHandler
+   */
+  function setActiveHandler() {
+    setActive(!onActive);
+    onActiveHandler(onActive);
+  }
+
   return (
-    <button type="button">Hello!1</button>
+    <button type="button" class={`${(onActive) ? 'on' : ''}`} onClick={setActiveHandler}>
+      {title}
+    </button>
   );
+}
+
+Button.propTypes = {
+  /** 버튼의 title 값 */
+  title: PropTypes.string,
+  active: PropTypes.bool,
+  onActiveHandler: PropTypes.func,
+}
+
+Button.defaultProps = {
+  title: '안녕하세요',
+  active: false,
+  onActiveHandler: () => {},
 }
 
 export default Button;
